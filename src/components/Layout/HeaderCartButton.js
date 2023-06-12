c3293b537ada6fcd81fc86850118805951e1d3ba
Paolo <paolo.mofardin94@gmail.com>
2023-06-12 17:42:46 +0200

    added logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContex from "../../store/cart-contex";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlited, setBtnIsHighlited] = useState(false);
+    const cartCtx = useContext(CartContex);
+
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
+    const btnClasses = `${classes.button} ${
+        btnIsHighlited ? classes.bump : ""
+    }`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlited(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlited(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,22 @@
-import MealItemForm from "./MealItemForm";
+import { useContext } from "react";
 
+import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContex from "../../../store/cart-contex";
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContex);
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCardHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price,
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -13,7 +26,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm id={props.id} onAddToCart={addToCardHandler} />
             </div>
         </li>
     );
